0eb1b6ab8c46b72792ce10453f42dccf3a8fc4a8
Abhishek Kumar Bauri <abhishekbauri12@gmail.com>
2024-02-02 16:18:48 +0530

    minor updates

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 //for uploading from form
-const fileUpload = require('express-fileupload');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
+const fileUpload = require("express-fileupload");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
 
-const app = express()
+require("dotenv").config({ path: "./config.env" });
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-require('dotenv').config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(expressLayouts);
 
-app.use(express.urlencoded({extended : true}));
-app.use(express.static('public'));
-app.use(expressLayouts)
-
-
-app.use(cookieParser('cookingBlogSecure'));
-app.use(session({
-    secret: 'cookingBlogSecretSession',
+app.use(cookieParser("cookingBlogSecure"));
+app.use(
+  session({
+    secret: "cookingBlogSecretSession",
     saveUninitialized: true,
-    resave: true
-}));
+    resave: true,
+  })
+);
 app.use(flash());
 app.use(fileUpload());
 
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
-
-app.set('layout', './layouts/main')
-app.set('view engine', 'ejs')
-
-const routes = require('./server/routes/recipeRoutes.js');
+const routes = require("./server/routes/recipeRoutes.js");
 // const cookieParser = require('cookie-parser');
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`listenig to port ${port}`)
-})
+  console.log(`listenig to port ${port}`);
+});
